6aaed103ce0e2ba8d415afa97ba10084ca341d02
ChristofferHall <christoffer.hall@brightstraining.com>
2022-11-25 08:51:02 +0100

    Added shoot method Added monster creation and hit detection

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,7 +8,6 @@ import com.googlecode.lanterna.terminal.Terminal;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -33,16 +32,14 @@ public class Main {
         final char player = FACE_WHITE;
         char monsterPlayer = '\uDC7D';
         String boom = "Game over";
-
         Character direction = 'd';
         int count = 0;
         Timer timer = new Timer();
 
-        ArrayList<Position> walls = CreateWalls(terminal);
-        Position monsterPosition = CreateMonster(terminal);
+        ArrayList<Position> walls = new ArrayList<>();
 
-        Score score =new Score();
-        createScore(terminal, score);
+//        walls = CreateWalls(terminal);
+        Position monsterPosition = CreateMonster(terminal);
 
         terminal.setCursorPosition(column, row);
         terminal.putCharacter(player);
@@ -132,12 +129,7 @@ public class Main {
             terminal.setCursorVisible(false);
             terminal.flush();
 
-
             if (column == monsterPosition.column && row == monsterPosition.row) {
-
-
-
-        }
                 for (int i = 0; i < boom.length(); i++) {
                     terminal.setCursorPosition(monsterPosition.column + i, monsterPosition.row);
                     terminal.putCharacter(boom.charAt(i));
@@ -145,6 +137,15 @@ public class Main {
 
                 }
             }
+
+
+
+//            count++;
+//
+//            if (count > 5) {
+//                walls.add(CreateMonster(terminal));
+//                count = 0;
+//            }
 
             // Shoot method call
             if (c == Character.valueOf('k')){
@@ -201,47 +202,6 @@ public class Main {
         return monsterPosition;
     }
 
-    public static void shoot(Terminal terminal, int x, int y, Character direction) throws IOException, InterruptedException {
-        TerminalSize t = terminal.getTerminalSize();
-
-        for (int i = 2; i < t.getColumns(); i++) {
-            switch (direction) {
-                case 'w' -> {
-                    terminal.setCursorPosition(x, y - i);
-                    terminal.putCharacter(TRIANGLE_UP_POINTING_BLACK);
-                    terminal.flush();
-                    terminal.setCursorPosition(x, y - i + 1);
-                    terminal.putCharacter(' ');
-                    terminal.flush();
-                }
-                case 's' -> {
-                    terminal.setCursorPosition(x, y + i);
-                    terminal.putCharacter(TRIANGLE_DOWN_POINTING_BLACK);
-                    terminal.flush();
-                    terminal.setCursorPosition(x, y + i - 1);
-                    terminal.putCharacter(' ');
-                    terminal.flush();
-                }
-                case 'a' -> {
-                    terminal.setCursorPosition(x - i, y);
-                    terminal.putCharacter(TRIANGLE_LEFT_POINTING_BLACK);
-                    terminal.flush();
-                    terminal.setCursorPosition(x - i + 1, y);
-                    terminal.putCharacter(' ');
-                    terminal.flush();
-                }
-                case 'd' -> {
-                    terminal.setCursorPosition(x + i, y);
-                    terminal.putCharacter(TRIANGLE_RIGHT_POINTING_BLACK);
-                    terminal.flush();
-                    terminal.setCursorPosition(x + i - 1, y);
-                    terminal.putCharacter(' ');
-                    terminal.flush();
-                }
-            }
-            Thread.sleep(3);
-        }
-    }
 
 
 }
